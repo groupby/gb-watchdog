b984e7925e146e5430d59ad4df0524008b8572bf
Tahir Wasim <tahir.wasim@groupbyinc.com>
2017-07-11 09:52:45 -0400

    fix gulp coveralls issue

--- tests/fakeE2ETests/onlyOneTestFails.js
+++ tests/fakeE2ETests/onlyOneTestFails.js
@@ -1,20 +1,19 @@
-const expect     = require('chai').expect;
-const _          = require('lodash');
+const expect = require('chai').expect;
+const _      = require('lodash');
 
 const keyObject = {
   anthropologie: {},
-  systemax: {},
-  novica: {},
-  austinkayak: {},
+  systemax:      {},
+  novica:        {},
+  austinkayak:   {},
 };
 
 _.forEach(keyObject, (value, customerId) => {
-  describe(`${customerId}`, function () {
-    this.timeout(20000);
-      it('assert test', () => {
-          if(customerId === "novica") {
-            expect(true).to.eql(false);
-          }
-      });
+  describe(`${customerId}`, () => {
+    it('assert test', () => {
+      if (customerId === "novica") {
+        expect(true).to.eql(false);
+      }
+    }).timeout(20000);
   });
 });
